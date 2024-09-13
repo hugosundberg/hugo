c2bf64e40876d16478e51c75346dd7d332d1e323
hugosundberg <hugosundberg98@gmail.com>
2024-09-13 15:18:58 +0200

    restart

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+
+    console.log("works");
 });
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
