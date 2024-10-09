f82e242e8d52746bdd6a93637453d5ffa91ba3e4
hugosundberg <hugosundberg98@gmail.com>
2024-10-09 14:38:51 +0200

    light/dark mode fixed

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const lightModeToggle = document.querySelector(".dark-light-mode-toggle");
+const githubLogo = document.querySelector(".github-logo");
+
+let toggle = false;
+const body = document.body;
+const darkBackgroundColor = body.style.backgroundColor;
+
+lightModeToggle.addEventListener("click", () => {
+  toggle = !toggle;
+  if (toggle) {
+    // Light Mode
+    githubLogo.classList.toggle("white-filter");
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+  } else {
+    // Dark mode
+    githubLogo.classList.toggle("white-filter");
+    body.style.backgroundColor = darkBackgroundColor;
+    body.style.color = "white";
+  }
+});
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
